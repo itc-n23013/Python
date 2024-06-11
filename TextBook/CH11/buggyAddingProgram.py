f6fced1f9b3c15c77603fae9eecf3ffaf357797d
itc-n23013 <n23013@std.it-college.ac.jp>
2024-06-11 04:47:00 +0000

    kadai

---
+++ TextBook/CH11/buggyAddingProgram.py
@@ -0,0 +1,9 @@
+print('足し合わせる第1の数を入力してください:')
+first = int(input())
+print('足し合わせる第2の数を入力してください:')
+second = int(input())
+print('足し合わせる第3の数を入力してください:')
+third = int(input())
+
+total = first + second + third 
+print('合計は' + str(total))
